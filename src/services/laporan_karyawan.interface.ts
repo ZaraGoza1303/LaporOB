c92363b3851ec0d736adf276889abb8464603587
FlintHassel <akmalcode843@gmail.com>
2026-07-03 09:18:00 +0700

    Del: laporan sblum nya

--- src/services/laporan_karyawan.interface.ts
+++
@@ -1,8 +0,0 @@
-import type { CreateLaporanReq } from "../dto/laporan.js"
-
-export interface ILaporanKaryawanService {
-  createLaporan(pelapor_id: string, data: CreateLaporanReq): Promise<any>;
-  getAllLaporan(): Promise<any[]>;
-  getDetailLaporan(id: string): Promise<any>;
-  updateStatusLaporan(laporanId: string, obId: string, statusBaru: string): Promise<any>;
-}
