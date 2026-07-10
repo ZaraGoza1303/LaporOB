2324b810ff08f89bb3922471ac9810d1137e677d
ZaraGoza1303 <mantapucoco@gmail.com>
2026-07-10 11:00:01 +0700

    fix: add type declarations for req.files and req.user

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,12 +1,31 @@
-declare global {
-    namespace Express {
-        interface Request {
-            user?: {
-                id: string, 
-                role: string,
-            }
-        }
-    }
+import { Readable } from "stream";
+
+interface AuthUser {
+  id: string;
+  username: string;
+  role: string;
 }
 
-export {}
+declare global {
+  namespace Express {
+    interface Request {
+      user?: AuthUser;
+      files?: Multer.File[] | { [fieldname: string]: Multer.File[] };
+    }
+
+    namespace Multer {
+      interface File {
+        fieldname: string;
+        originalname: string;
+        encoding: string;
+        mimetype: string;
+        size: number;
+        stream: Readable;
+        destination: string;
+        filename: string;
+        path: string;
+        buffer: Buffer;
+      }
+    }
+  }
+}
